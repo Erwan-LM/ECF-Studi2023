2ca9a6525deb13824674c9831eaaf5628a462ab3
LE MEUR Erwan <110557085+Erwan-LM@users.noreply.github.com>
2023-07-17 18:03:24 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'localhost';
-$database = 'garage_ecf';
-$emailBD = 'root';
-$password = 'root';
+$host = $_ENV['DB_HOST'];
+$database = $_ENV['DB_NAME'];
+$emailBD = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 // Connexion à la base de données
 $connection = new PDO("mysql:host=$host;dbname=$database", $emailBD, $password);
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
